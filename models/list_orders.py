2f95023a24220bb47208e9261e3bd91d7ddd2b21
Student <student@Students-MacBook-Pro.local>
2023-05-31 16:07:04 +0100

    css extension

--- models/list_orders.py
+++ models/list_orders.py
@@ -3,4 +3,4 @@ from models.order import *
 order1 = Order("Ash", "31/05/23", "Gibson Les Paul", 2)
 order2 = Order("Randolph", "31/05/23", "Kazoo", 65)
 orders = [order1, order2]
-
+# datetime.date()
